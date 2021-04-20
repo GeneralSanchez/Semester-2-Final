2b78a3efa5ab459dd6552c94cd81c6d3ca26069a
Andy Sanchez <>
2021-04-20 06:08:10 +0000

    Added arraylist to the main

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
 class Main 
 {
   public static void main(String[] args) {
     System.out.println("Andy Sanchez, Semester 2 Final Project");
+    ArrayList<Startrek> Trek = new ArrayList<Startrek>();
+    ArrayList<Starwars> Wars = new ArrayList<Starwars>();
+    for (Startrek i: Trek)
+    {
+      for(Starwars j: Wars)
+      {
+       System.out.println(i);
+      System.out.println(Andy.Singing(j));
+      System.out.println (i);
+      System.out.println();
   }
 }

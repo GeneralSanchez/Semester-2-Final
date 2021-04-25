2c68a78b7f1cf51e38615cbcc1295de209cf60b9
Andy Sanchez <>
2021-04-25 22:04:18 +0000

    Added the JOption so that they work with the classes

--- Main.java
+++ Main.java
@@ -4,31 +4,59 @@ class Main
 {
   public static void main(String[] args) {
     System.out.println("Andy Sanchez, Semester 2 Final Project");
-    ArrayList<Startrek> Trek = new ArrayList<Startrek>();
-    ArrayList<Starwars> Wars = new ArrayList<Starwars>();
-//Need to change this later
-  String num1 = JOptionPane.showInputDialog("Enter first integer");
-  String num2 = JOptionPane.showInputDialog("Enter first integer");
-  String num3 = JOptionPane.showInputDialog("Enter first integer");
+    System.out.println();
+    //The Arrays that are being used
+//     ArrayList<Startrek> Trek = new ArrayList<Startrek>();
+//     ArrayList<Starwars> Wars = new ArrayList<Starwars>();
+// //This everething for Star Trek Class
+//   String num1 = JOptionPane.showInputDialog("Enter Name of Star Trek Character");
+//   String num2 = JOptionPane.showInputDialog("Years with Star Trek be honest!");
+//   String num3 = JOptionPane.showInputDialog("What is the rating you give Star Trek");
+//   int parsedNum2 = Integer.parseInt(num2);
+//   double parsedNum3 = Double.parseDouble(num3);
+//   Startrek evil = new Startrek (num1,parsedNum2,parsedNum3);
+//   Trek.add(evil);
+// //This is all the user inputs from the parent class which will be reused in the children classes
+//   String nam1 = JOptionPane.showInputDialog("Enter a Star Wars Character Name!");
+//   String nam2 = JOptionPane.showInputDialog("Enter a Star Wars Rank!");
+//   String nam3 = JOptionPane.showInputDialog("Are you a extrmist fan");
+//   String nam4 = JOptionPane.showInputDialog("Enter in a random ID with numbers");
+//   String nam5 = JOptionPane.showInputDialog("Have you thought about being in any side is Star Wars");
+//   String nam6 = JOptionPane.showInputDialog("Namer the weapon you gonna use");
+//   boolean parsedNam3 = Boolean.parseBoolean (nam3);
+//   int parsedNam4 = Integer.parseInt(nam4);
+//   boolean parsedNam5 = Boolean.parseBoolean (nam5);
+//   //This is for the light side variables
+//   String nem1 = JOptionPane.showInputDialog("Enter the Army that you like from Star Wars1");
+//   String nem2 = JOptionPane.showInputDialog("Enter the year that army is from!");
+//   String nem3 = JOptionPane.showInputDialog("Enter the ship from that army that you love");
+//   double parsedNem2 = Double.parseDouble (nem2);
 
-  Startrek evil = new Startrek (num3);
-  Trek.add(num3);
-  Wars.add(num1);
-  Wars.add(num2);
-  
-
-  
+//   //Jedi light = new Jedi (nam1,nam2,parsedNam3,parsedNam4,parsedNam5,nam6);
+//   //Sith dark = new Sith (nam1,nam2,parsedNam3,parsedNam4,parsedNam5,nam6);
+//   Army universe = new Army (nam1,nam2,parsedNam3,parsedNam4,parsedNam5,nam6,nem1,parsedNem2,nem3);
+//   Wars.add(universe);
 
 
-    for (Startrek i: Trek)
-    {
-      for(Starwars j: Wars)
-      {
-       System.out.println(i);
-      System.out.println(evil.Singalong(j));
-      System.out.println (i);
-      System.out.println();
-      }
-    }
+
+//     for (Startrek i: Trek)
+//     {
+//       for(Starwars j: Wars)
+//       {
+//       System.out.println(i);
+//       System.out.println();
+//       System.out.println(evil.Singalong(j));
+//       System.out.println();
+//       System.out.println (i);
+//       System.out.println();
+//       }
+//     }
+    System.out.println (memorablending ("May the force be with you!"));
+    System.out.println (memorablending("It has been some two amazing years in my CS classes,and as such good things they must come to an end to ealy "));
+  }
+  public static String memorablending(String comment)
+  {
+  String a = comment.toUpperCase().replace ("A","@");
+  return a;
   }
 }
--- Startrek.java
+++ Startrek.java
@@ -5,32 +5,28 @@ private String name;//String variable
 private int years;// int variable
 private double rating;// double variable
 //these are the getter ans setters
-public Startrek(String name)//sets name
+public Startrek(String name,int years,double rating)//sets name
 {
   this.name = name;//sets this 
-}
-public Startrek (int years)//sets years
-{
   this.years = years;//sets this 
-}
-public Startrek (double rating)//sets rating
-{
   this.rating = rating;//sets this 
 }
+
+
 public String Singalong(Starwars Wars)
 {
   return "With a "+ Wars.getWeapon()+"on my hand and force on my side"+
-        "I'll turn this tragegic Star Trek into dust"+
-        "I give those forgotten Star Trek Fans a chance to be reborn"+
-        "I hear their cries and alllow to tranform to"+ Wars.getName()+
-        "Soon Star Trek will be destoryed,Soon its Star Wars time";
+        "\nI'll turn this tragegic Star Trek into dust"+
+        "\nI give those forgotten Star Trek Fans a chance to be reborn"+
+        "\nI hear their cries and alllow to tranform to"+ Wars.getName()+
+        "\nSoon Star Trek will be destoryed,Soon its Star Wars time";
 }
 public String toString()
 {
   String output = name+",I will win and I will rise "+
-      "All of Star Trek Destroyed "+
-      "No more Star Trek and no more science"+
-      "Star Wars will be number one";
+      "\nAll of Star Trek Destroyed "+
+      "\nNo more Star Trek and no more science"+
+      "\nStar Wars will be number one";
       return output;
 }
 
